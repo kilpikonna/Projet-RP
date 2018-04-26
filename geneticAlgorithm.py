b334d00b0e69469e69a6525a5f146c25ca4f0481
Yasmine <hamdaniyasmine15@gmail.com>
2018-04-27 01:10:36 +0200

    GeneticAlgo working on B with random init but often cant find opt

--- geneticAlgorithm.py
+++ geneticAlgorithm.py
@@ -50,7 +50,7 @@ def KruskalOnSubset(adjacencyMatrix, nodesSubset):
     
     arcs = sorted(arcs.items(),key = operator.itemgetter(1))
     
-    print("arcs : ",arcs," and nodes : ",nodesSubset)    
+    #print("arcs : ",arcs," and nodes : ",nodesSubset)    
     keys = [i[0] for i in arcs]
     values = [i[1] for i in arcs]
     spanningTree = dict()
@@ -81,9 +81,9 @@ def createRandomInitPopulation(individualSize, populationSize):
         for j in range(0, individualSize) :
             individual[j] = np.random.choice(np.arange(2), p=[1-p, p])
         population.append(individual)
+        nb = [i for i in individual].count(0)
+        print("number of zeros : ", nb)
     
-        print(p," population[i] ===", population[i])
-        
     return population
     
 """
@@ -98,21 +98,23 @@ def createRandomInitPopulation(individualSize, populationSize):
 
 """
 
-def calculateRealIndex(terminals, i) :
+def calculateRealIndex(terminals, index) :
     terminals.sort()
-    index = i
+#    print(terminals)
+    i = index
+#    index = i
     for terminal in terminals :
-        if terminal <= i :
+        if terminal <= index :
+#            print("terminal : ",terminal," is lower than ",index)
             index+=1
-        else :
-            
+        else :            
             break
     
-    print("i==",i," but real index =",index)
+#    print("i==",i," but real index =",index)
     return index
 
 def solutionToIndividual(solution, terminals, individualSize) :
-    print("individual Size is :::::::::::::::",individualSize)
+    #print("individual Size is :::::::::::::::",individualSize)
     nonTerminal_Nodes = set([])
     for arc in solution :
         if arc[0] not in terminals :
@@ -132,14 +134,20 @@ def solutionToIndividual(solution, terminals, individualSize) :
 def createHeuristicInitPopulation(adjacencyMatrix, terminals, populationSize):
     
     population = []
-    for i in range(populationSize) :
+#    for i in range(int(populationSize/2)) :
+    for i in range(int(populationSize)) :
         newAdjacencyMatrix = inputGraphRandomization(adjacencyMatrix, 5, 20)
         solution = ACMHeuristic(newAdjacencyMatrix, terminals)
         individual = solutionToIndividual(solution.keys(), terminals, len(adjacencyMatrix)-len(terminals))
         population.append(individual)
-        print(">new individual :::: ", individual, "\n>from solution : ", solution.keys()," \n>terminals : ", terminals)#, " type is :::: ", individual.shape)
+        
+#        solution = distanceGraphHeuristic(newAdjacencyMatrix, terminals)
+#        individual = solutionToIndividual(solution.keys(), terminals, len(adjacencyMatrix)-len(terminals))
+#        population.append(individual)
+        
+        #print(">new individual :::: ", individual, "\n>from solution : ", solution.keys()," \n>terminals : ", terminals)#, " type is :::: ", individual.shape)
     
-    print("population :::::", population)
+    #print("population :::::", population)
     return population
 
 """
@@ -154,16 +162,17 @@ def generateSubgraph(terminals, individual, adjacencyMatrix):
     
     subGraphNodes = []
     for terminal  in terminals :
+#        print(terminal)
         subGraphNodes.append(int(terminal))
+#        print("subGraph", subGraphNodes)
     
     for i in range(0, len(individual)) :
-        if individual[i]==1 : # Steiner node
-            
+        if individual[i]==1 : # Steiner node           
             realIndex = calculateRealIndex(terminals, i)
-            print(subGraphNodes," and the node is : ", i," but the real value is : ",realIndex) 
+            #print(subGraphNodes," and the node is : ", i," but the real value is : ",realIndex) 
             subGraphNodes.append(int(realIndex))
     
-    print("final subGraphNodes : ", subGraphNodes)
+    #print("final subGraphNodes : ", subGraphNodes)
     return subGraphNodes
 
 """
@@ -179,12 +188,13 @@ def generateSubgraph(terminals, individual, adjacencyMatrix):
 def kruskal_fitness(adjacencyMatrix, nodesSubset, M) :
     kruskal_trees = KruskalOnSubset(adjacencyMatrix, nodesSubset)
     fitness = 0
-    print("Tree : ",kruskal_trees)    
+    #print("Tree : ",kruskal_trees)    
     for arc in kruskal_trees.keys() :
         fitness = fitness + kruskal_trees.get(arc)
     
     nbNodes = len(nodesSubset)
     nbEdges = len(kruskal_trees)
+    
     if nbEdges == nbNodes-1 :# Tree    
         return fitness
     else :
@@ -206,11 +216,13 @@ def kruskal_fitness(adjacencyMatrix, nodesSubset, M) :
 
 def fitness(terminals, individual, adjacencyMatrix, M):
     nodesSubset = generateSubgraph(terminals, individual, adjacencyMatrix)
-#    print(nodesSubset)    
+    nodesSubset.sort(key=int)
+
     weights = kruskal_fitness(adjacencyMatrix, nodesSubset, M)   
+    
     return weights
    
-
+# 25 23 19 9 7 11 39 6 41 40 28 43 49
 """
 @param : fitnessList : dictionary containing ...
 
@@ -218,26 +230,30 @@ def fitness(terminals, individual, adjacencyMatrix, M):
 @return : two parents selected proportionally to their fitness using cumulative prob
 
 """      
-def rouletteWheelSelection(fitnessList) : # select parents
+def rouletteWheelSelection(fitnessList, fitnessSum) : # select parents
         
     # select a random a number in the range [0,1]
-    random_num1=random.random()
-    random_num2=random.random()
+    random_num1=random.uniform(0,fitnessSum)#.random() #.randint(0, fitnessSum)
+    random_num2=random.uniform(0,fitnessSum)#.random() #(0, fitnessSum)
     parent1 = None
     parent2 = None
+    partialSum = 0
     
-    for individual in fitnessList.keys() :
-        if fitnessList[individual][3] > random_num1 and parent1 == None :
-            parent1 = individual
-        if fitnessList[individual][3] > random_num2 and parent2 == None :    
-            parent2 = individual
+#    parent1 = fitnessList[0][0]
+#    parent2 = fitnessList[1][0]
+    
+    
+    for i in range(len(fitnessList)) :
+        partialSum = partialSum + fitnessList[i][1]
+        if partialSum >= random_num1 and parent1 == None :
+            parent1 = fitnessList[i][0]
+        if partialSum >= random_num2 and parent2 == None :    
+            parent2 = fitnessList[i][0]
         if parent1 != None and parent2 != None :
             break
-        
+    
     return parent1, parent2
 
-#def parentSelection(fitnessList, selectionType) :
-#    return None
 
 """
 @param : parent1
@@ -256,7 +272,9 @@ def onePoint_crossover(parent1, parent2) :
     child2 = [parent2[i] for i in range(index)]
     child2.extend(parent1[i] for i in range(index, len(parent1)))
     
-    print("-----------------------------------", child1)
+    child1 = np.array(child1)
+    child2 = np.array(child2)
+    #print("-----------------------------------", child1)
     return child1, child2
     
 
@@ -268,11 +286,98 @@ def onePoint_crossover(parent1, parent2) :
 """  
 def bitFlip_mutation(child) : 
     perform_mutation = random.random()
-    if(perform_mutation<=1/10) :
+    if(perform_mutation<=4/100) :
         index = random.randint(0, len(child)-1)
         child[index] = (child[index]+1)%2
 
 
+def bestIndividuals(fitnessList, populationSize) :
+#    fitnessList.sort(key=operator.itemgetter(1))
+#    sorted_fitnessList = sorted(fitnessList.items(), key=operator.itemgetter(1))
+    
+    m = len(fitnessList)
+    
+    population = [np.array(fitnessList[i][0]) for i in range(min(populationSize, m))]
+    
+    return population
+
+def getCategories(population) :
+    distinctIndividuals = set()
+    for individual in population :
+        distinctIndividuals.add(tuple(individual))
+        
+    return distinctIndividuals
+
+def notIn(oldPopulation, individual) :
+    for individual2 in oldPopulation :
+#        print("1 :", type(individual),"  2:",type(individual2))
+        if (individual2 == individual).all() :
+                return 0.
+    return 1.
+
+def getNbOfNewIndividuals(oldPopulation, newPopulation) :
+    new = 0.
+    for individual in newPopulation :
+        new += notIn(oldPopulation, individual)
+    
+    return new
+
+def bestDistinctIndividuals(fitnessList, populationSize) :
+    m = len(fitnessList)
+    
+    population = [fitnessList[i][0] for i in range(len(fitnessList))]
+    
+    
+    populationSet = set(population)
+    
+    if len(populationSet) < populationSize :
+        return population[:populationSize]
+    else :        
+        bestIndvs = [np.array(individual) for individual in populationSet]
+        return bestIndvs[:populationSize]
+    
+
+def elitisimBasedSurvivorSelection(fitnessList, fitnessSum, n) :
+    fitnessList.sort(key=operator.itemgetter(1))    
+    for i in range(int(n/2)) :
+        # selection : select two parents (individuals) among the population 
+        parent1, parent2 = rouletteWheelSelection(fitnessList.copy(), fitnessSum)              
+        # crossover : we select a random crossover point and exchange the tails of the parents to get 2 new individuals
+        child1, child2 = onePoint_crossover(parent1, parent2)
+        # mutation : exchange the value of a random node
+        bitFlip_mutation(child1)
+        bitFlip_mutation(child2)
+        # insertion : insert new individuals to the population
+        tchild1 = tuple(child1) 
+        fit = fitness(terminals, tchild1, adjacencyMatrix, M)
+        fitnessList.append([tchild1, fit, 1/fit]) #, 0, 0]
+        
+        tchild2 = tuple(child2) 
+        fit = fitness(terminals, tchild2, adjacencyMatrix, M)
+        fitnessList.append([tchild2, fit, 1/fit])#, 0, 0]
+        
+        
+    fitnessList.sort(key=operator.itemgetter(1))
+    bestIndvs = bestIndividuals(fitnessList, n)
+#    bestIndvs = bestDistinctIndividuals(fitnessList, n)
+    return bestIndvs
+
+def childrenBasedSurvivorSelection(fitnessList, fitnessSum, n) :
+    population = []
+    for i in range(int(n/2)) :
+        # selection : select two parents (individuals) among the population 
+        parent1, parent2 = rouletteWheelSelection(fitnessList.copy(), fitnessSum)              
+        # crossover : we select a random crossover point and exchange the tails of the parents to get 2 new individuals
+        child1, child2 = onePoint_crossover(parent1, parent2)
+        # mutation : exchange the value of a random node
+        bitFlip_mutation(child1)
+        bitFlip_mutation(child2)
+        # insertion : insert new individuals to the population
+        population.append(child1)
+        population.append(child2)
+        
+    return population
+        
 """
 @param : adjacencyMatrix : 
 
@@ -285,71 +390,86 @@ def bitFlip_mutation(child) :
 @return : the best solution found with genetic algorithm
 
 """  
-def simpleGeneticAlgorithm(adjacencyMatrix, terminals, n, M) :
+def simpleGeneticAlgorithm(adjacencyMatrix, terminals, populationSize, n, M) :
     
-#    population = createRandomInitPopulation(len(adjacencyMatrix)-len(terminals), n)
-    population = createHeuristicInitPopulation(matrix, [0,1], n)
-    
+    population = createRandomInitPopulation(len(adjacencyMatrix)-len(terminals), populationSize)
+#    population = createHeuristicInitPopulation(adjacencyMatrix, terminals, populationSize)
+    fitnessList = []
     for k in range(n) :
         
         print("********************************* k=",k)
-        print("*************population : ",population)
-        fitnessList = dict()
+        print("*************population size : ",len(population))
+#        print(population)
+        print("*******************categories : ", len(getCategories(population)))
+        fitnessList = []
         fitnessSum = 0
         
-        # calculate and adapts fitness to fit the rule : the more the fit the more the individual's prob is
-        for i in range(n) :
-            individual = tuple(population[i])
-            print("populasse : ",population[i])
-            fit = fitness(terminals, individual, adjacencyMatrix, M)
-            fitnessList[individual] = [fit, 1/fit, 0, 0] # {individual : [fitness, new fitness, probability, cumprob]}
-            fitnessSum += fitnessList[individual][1]
-        # calculates the probability of each individual + cumulative prob
-        cumprob = 0
-        for i in range(n) :
-            individual = tuple(population[i])
-            prob = fitnessList[individual][1]/fitnessSum
-            fitnessList[individual][2] = prob
-            cumprob +=  prob
-            fitnessList[individual][3] = cumprob
-            
-        population = []
-            
-        for i in range(int(n/2)) :
-            # selection : select two parents (individuals) among the population 
-            parent1, parent2 = rouletteWheelSelection(fitnessList)              
-            # crossover : we select a random crossover point and exchange the tails of the parents to get 2 new individuals
-            child1, child2 = onePoint_crossover(parent1, parent2)
-            # mutation : exchange the value of a random node
-            bitFlip_mutation(child1)
-            bitFlip_mutation(child2)
-            # insertion : insert new individuals to the population
-            population.append(child1)
-            population.append(child2)
-       
         
+        # calculate and adapts fitness to fit the rule : the more the fit the more the individual's prob is
+        for i in range(populationSize) :
+            individual = tuple(population[i])
+            fit = fitness(terminals, individual, adjacencyMatrix, M)
+            fitnessList.append([individual, fit, 1/fit])#, 0, 0] # {individual : [fitness, new fitness, probability, cumprob]}
+            fitnessSum += 1/fit
+
+        newPopulation = elitisimBasedSurvivorSelection(fitnessList, fitnessSum, populationSize)
+        print(">>>>>>>>>>>>>>>> new Individuals : ",getNbOfNewIndividuals(population, newPopulation))
+        population = newPopulation
+        bestIndividual = []
+
+        for individual in fitnessList :
+            if len(bestIndividual) == 0 or bestIndividual[1] > individual[1] :
+                bestIndividual = [individual[0], individual[1]]
+            i += 1
+        print("******************************************* k ====",k," bestFit :", bestIndividual[1], 
+              "\nlen of the solution : ", len(bestIndividual[0]), " and len of terminals : ",len(terminals))
            
         
-#        population = set(population) # is a good idea ?
-        
-        
     bestIndividual = []
-    for individual in fitnessList.keys() :
-#        individual = fitnessList[i]
-        if len(bestIndividual) == 0 or bestIndividual[1] > fitnessList[individual][0] :
-            bestIndividual = [individual, fitnessList[individual][0]]
+    for individual in fitnessList :
+        if len(bestIndividual) == 0 or bestIndividual[1] > individual[1] :
+            bestIndividual = [individual[0], individual[1]]
     
     return bestIndividual
         
 
-#adjacencyMatrix, terminals = readInstance(os.getcwd()+"\heuristic\instance039.gr")
+adjacencyMatrix, terminals = readInstance(os.getcwd()+"\instances\B\\b02.stp")
 
 M = 100000*100000
-n = 5
+n = 50
+populationSize = 100
+#iterations = 50
 
+#terminals = [20, 18, 24]
+#individual = np.zeros((37))
+
+#print(fitness(terminals, individual, adjacencyMatrix,M))
+#print(terminals)
 #print(adjacencyMatrix, terminals)
-#print(simpleGeneticAlgorithm(adjacencyMatrix, terminals, n, M))
 
+
+print(simpleGeneticAlgorithm(adjacencyMatrix, terminals, populationSize, n, M))
+
+
+#print()
+#print(fitness(terminals, np.array(([0,0,0,0,1,1,0,0,1,0,0,0,0,1,0,1,0,1,0,0,1,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,0])), adjacencyMatrix,M))
+
+
+#Codage:  [1-0, 2-0, 3-0, 4-0, 5-0, 8-0, 10-0, 12-0, 13-0, 14-0, 15-0, 16-0, 17-0, 18-0, 20-1, 21-0, 22-0, 
+#          24-1,1, 27-1, 29-1, 30-0, 31-0, 32-0, 33-0, 34-0, 35-1, 36-1, 37-1, 38-0, 42-0,  44-1,
+#          45-1, 46-0, 47-1, 48-0, 50-0]
+#         
+#Liste de sommets :
+# ['u22', 'u1', 'u15', 'u38',
+#  'u45', 'u27', 'u18', 'u48', 'u36', 'u3', 'u8', 'u21', 'u17', 'u24', 'u50', 'u37', 'u33', 'u35', 'u5', 'u42', 'u20',
+#  'u46', 'u26', 'u29', 'u14', 'u16', 'u30', 'u31', 'u47', 'u44', 'u10', 'u2', 'u13', 'u34', 'u4', 'u32', 'u12']
+
+#parent1 = [0,1,0,0,0,0,0,0]
+#parent2 = [1,0,1,1,1,1,1,1]
+#
+#print(onePoint_crossover(parent1, parent2))
+#bitFlip_mutation(parent1)
+#print(parent1)
 
 matrix = np.zeros((5,5))
 matrix[0] = [0,0,2,1,5]
@@ -359,9 +479,9 @@ matrix[3] = [1,4,3,0,2]
 matrix[4] = [5,2,1,2,0]
 #parent1 = [0,0,1,1,0]
 #parent2 = [0,0,0,1,1]
-n=4
+#n=100
 
-print(createHeuristicInitPopulation(matrix, [0,1], 10))
+#print(createHeuristicInitPopulation(matrix, [0,1], 10))
 
-print(simpleGeneticAlgorithm(matrix, [0,1], n, M))
+#print(simpleGeneticAlgorithm(matrix, [0,1], n, M))
 
